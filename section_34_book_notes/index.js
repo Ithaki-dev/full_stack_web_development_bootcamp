8c1f0cf83395787119b3b7fe5c44a05c1bfa2559
rquesada <rquesadaqq@outlook.com>
2025-07-23 19:23:48 -0600

    Add HTML structure and create endpoints

--- section_34_book_notes/index.js
+++ section_34_book_notes/index.js
@@ -28,3 +28,76 @@ db.connect(
     }
   }
 );
+
+ 
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/add", (req, res) => {
+  const { title, author, year } = req.body; 
+
+  db.query(
+    "INSERT INTO books (title, author, year) VALUES ($1, $2, $3)",
+    [title, author, year],
+    (err, result) => {
+      if (err) {
+        console.error("Error executing query", err.stack);
+        res.status(500).send("Error adding book");
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.get("/edit/:id", (req, res) => {
+  const bookId = req.params.id;
+  db.query("SELECT * FROM books WHERE id = $1", [bookId], (err, result) => {
+    if (err) {
+      console.error("Error executing query", err.stack);
+      res.status(500).send("Error fetching book");
+    } else {
+      res.render("edit", { book: result.rows[0] });
+    }
+  });
+});
+
+app.post("/edit/:id", (req, res) => {
+  const bookId = req.params.id;
+  const { title, author, year } = req.body;
+  db.query(
+    "UPDATE books SET title = $1, author = $2, year = $3 WHERE id = $4",
+    [title, author, year, bookId],
+    (err, result) => {
+      if (err) {
+        console.error("Error executing query", err.stack);
+        res.status(500).send("Error updating book");
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.get("/delete/:id", (req, res) => {
+  const bookId = req.params.id;
+  db.query("DELETE FROM books WHERE id = $1", [bookId], (err, result) => {
+    if (err) {
+      console.error("Error executing query", err.stack);
+      res.status(500).send("Error deleting book");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
