07f2c20b0cf8545eb3ffc17a76d643760c88843d
rquesada <rquesadaqq@outlook.com>
2025-08-01 23:30:54 -0600

    Add section 35 auth

---
+++ section_35_authentication/9.1+Authentication+Lv.1/index.js
@@ -0,0 +1,99 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "admin123",
+  port: 5432,
+});
+
+db.connect(
+  (err) => {
+    if (err) {
+      console.error("Database connection error:", err);
+    } else {
+      console.log("Connected to the database");
+    }
+  }
+);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  // save user data to the database
+  const email = req.body.username;
+  const password = req.body.password;
+
+  const checkUserExists = await db.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email]
+  );
+
+  try {
+    if (checkUserExists.rows.length > 0) {
+      console.log("User already exists");
+      res.redirect("/register");
+    } else {
+      await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
+      );
+      console.log("User registered successfully");
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).send("Error registering user");
+  }
+});
+
+app.post("/login", async (req, res) => {
+
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const result = await db.query(
+      "SELECT * FROM users WHERE email = $1 AND password = $2",
+      [email, password]
+    );
+    if (result.rows.length > 0) {
+      console.log("User logged in successfully");
+      res.redirect("/secrets");
+    } else {
+      console.log("Invalid credentials");
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).send("Error logging in user");
+  }
+});
+
+app.get("/secrets", async (req, res) => {
+  res.render("secrets.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
